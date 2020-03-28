7ea1f87a01bb90682bae53a369afcd5e365b46de
Konstantin Goncharik <botankras@gmail.com>
2020-03-29 01:51:52 +0700

    Formatted Point2D.h

--- src/Point2D.h
+++ src/Point2D.h
@@ -2,51 +2,51 @@
 #include <cmath>
 
 struct Point2D {
-	double x = 0;
-	double y = 0;
+    double x = 0;
+    double y = 0;
 
-	Point2D& operator+=(const Point2D& rhs)
-	{
-		this->x += rhs.x;
-		this->y += rhs.y;
-		return *this;
-	}
+    Point2D& operator+=(const Point2D& rhs)
+    {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        return *this;
+    }
 
-	Point2D& operator-=(const Point2D& rhs)
-	{
-		this->x -= rhs.x;
-		this->y -= rhs.y;
-		return *this;
-	}
+    Point2D& operator-=(const Point2D& rhs)
+    {
+        this->x -= rhs.x;
+        this->y -= rhs.y;
+        return *this;
+    }
 
-	Point2D& operator*=(double number)
-	{
-		this->x *= number;
-		this->y *= number;
-		return *this;
-	}
+    Point2D& operator*=(double number)
+    {
+        this->x *= number;
+        this->y *= number;
+        return *this;
+    }
 
-	double operator*(const Point2D& point2D) const { return x * point2D.x + y * point2D.y; }
+    double operator*(const Point2D& point2D) const { return x * point2D.x + y * point2D.y; }
 
-	double cross(const Point2D& rhs) const { return this->x * rhs.y - this->y * rhs.x; }
-	Point2D normalize() const { return Point2D{this->x / abs(), this->y / abs()}; }
-	double abs() const { return std::sqrt(x * x + y * y); }
+    double cross(const Point2D& rhs) const { return this->x * rhs.y - this->y * rhs.x; }
+    Point2D normalize() const { return Point2D { this->x / abs(), this->y / abs() }; }
+    double abs() const { return std::sqrt(x * x + y * y); }
 };
 
 inline Point2D operator+(Point2D lhs, const Point2D& rhs)
 {
-	lhs += rhs;
-	return lhs;
+    lhs += rhs;
+    return lhs;
 }
 
 inline Point2D operator-(Point2D lhs, const Point2D& rhs)
 {
-	lhs -= rhs;
-	return lhs;
+    lhs -= rhs;
+    return lhs;
 }
 
 inline Point2D operator*(Point2D lhs, double rhs)
 {
-	lhs *= rhs;
-	return lhs;
+    lhs *= rhs;
+    return lhs;
 }
