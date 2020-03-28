48ae73573c2b2a2a08d6c1361d2130185ee4fc1e
Konstantin Goncharik <botankras@gmail.com>
2020-03-28 21:32:26 +0700

    Moved Point2D to Point2D.h and Point2D.cpp

--- src/Object2D.h
+++ src/Object2D.h
@@ -3,57 +3,10 @@
 #include <utility>
 #include <vector>
 #include <cmath>
+#include "Point2D.h"
 #include "Idrawable.h"
 #include "settings.h"
 
-struct Point2D {
-    double x = 0;
-    double y = 0;
-
-	Point2D& operator+=(const Point2D& rhs)
-	{
-		this->x += rhs.x;
-		this->y += rhs.y;
-		return *this;
-	}
-
-	Point2D& operator-=(const Point2D& rhs)
-	{
-		this->x -= rhs.x;
-		this->y -= rhs.y;
-		return *this;
-	}
-
-	Point2D& operator*=(double number)
-	{
-		this->x *= number;
-		this->y *= number;
-		return *this;
-	}
-
-	double operator*(const Point2D& point2D) const { return x * point2D.x + y * point2D.y; }
-
-    Point2D normalize() { return Point2D{this->x/abs(), this->y/abs()};}
-    double abs() {return sqrt(x*x + y*y); }
-};
-
-inline Point2D operator+(Point2D lhs, const Point2D& rhs)
-{
-	lhs += rhs;
-	return lhs;
-}
-
-inline Point2D operator-(Point2D lhs, const Point2D& rhs)
-{
-	lhs -= rhs;
-	return lhs;
-}
-
-inline Point2D operator*(Point2D lhs, double rhs)
-{
-	lhs *= rhs;
-	return lhs;
-}
 
 class Object2D : virtual public Idrawable {
 protected:
